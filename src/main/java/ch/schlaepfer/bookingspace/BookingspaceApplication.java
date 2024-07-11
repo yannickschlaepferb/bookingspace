a43a5b32c64875bad3b1c56c42271add8628bafb
yannick <yannick.schlaepfer@lernende.bbw.ch>
2024-07-11 11:25:06 +0200

    initial commit

---
+++ src/main/java/ch/schlaepfer/bookingspace/BookingspaceApplication.java
@@ -0,0 +1,13 @@
+package ch.schlaepfer.bookingspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/schlaepfer/bookingspace/BookingspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.schlaepfer.bookingspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
